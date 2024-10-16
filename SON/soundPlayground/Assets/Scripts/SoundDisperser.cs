cea3207108a636a8666b65472c200e1677cfbdc6
ivo_hr <81875802+ivo-hr@users.noreply.github.com>
2024-10-16 19:44:49 +0200

    sew

--- SON/soundPlayground/Assets/Scripts/SoundDisperser.cs
+++ SON/soundPlayground/Assets/Scripts/SoundDisperser.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 [System.Serializable]
 public class SoundGroup {
+    [SerializeField, Tooltip("List of sounds that can be played")]
     public AudioClip[] sounds;
-    [Range(0f, 1f)]
+    [Range(0f, 1f), Tooltip("From what ambience Intensity the sound will be allowed to play")]
     public float randomThreshold;
-    [Range(1f, 10f)]
+    [Range(1f, 10f), Tooltip("Probability multiplier for this sound group in relation to the ambience Intensity")]
     public float probabilityMultiplier;
 }
 
@@ -18,12 +19,13 @@ public class Ambience {
     [Range(0f, 1f)]
     public float intensity;
 
+    [SerializeField, Tooltip("Sound groups that will be played in this ambience")]
     public List<SoundGroup> soundGroups;
 }
 public class SoundDisperser : MonoBehaviour {
     
-    [SerializeField]
-    public List<Ambience> soundEnvironment; //Sonido de fondo
+    [SerializeField, Tooltip("List of sound environments that include ambience sound and sound groups")]
+    public List<Ambience> soundEnvironments; //Sonido de fondo
 
 
 }
